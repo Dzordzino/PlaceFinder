d7e7bcd0205565384b39e8cea23d930983cbd35b
unknown <nikola.lazovic@quantox.com>
2019-09-03 13:43:15 +0200

    chage class to functional component

--- src/components/PlaceInfo.js
+++ src/components/PlaceInfo.js
@@ -1,45 +1,39 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 
 import List from "./List";
 
-class PlaceInfo extends Component {
-    
-    render() {
-        const { showPlaceInfo, selectedPlace, showList, data, showInfo } = this.props;
-        return (
+const PlaceInfo = ({ showPlaceInfo, selectedPlace, showList, data, showInfo }) => (
+    <Fragment>
+        { showPlaceInfo ? (
             <Fragment>
-                { showPlaceInfo ? (
-                    <Fragment>
-                        <img src=
-                        { selectedPlace.photos ? (
-                            selectedPlace.photos[0].getUrl()
-                        ) : (  selectedPlace.icon )}
-                        alt={selectedPlace.name +" image"} className="placeInfoImage" />
-                        <p className="placeInfoTitle">
-                            {selectedPlace.name}
-                            <span className={selectedPlace.opening_hours && selectedPlace.opening_hours.open_now ? "statusCircle" : "statusCircle closed"}></span>
-                        </p>
-                        <p className="placeInfoTitle">
-                            {selectedPlace.rating ? "rating: " + selectedPlace.rating : "No one rated this place"}
-                        </p>
-                        <p className="placeInfoTitle">
-                            {selectedPlace.vicinity ? selectedPlace.vicinity : "Unknown address"}
-                        </p>
-                        <button type="button" onClick={ showList }>Go to the list</button>
-                    </Fragment>
-                ) : (
-                    <Fragment>
-                        { data.places ? (
-                            <List data={data.places} showInfo={showInfo} />
-                            ):(
-                                ""
-                            )
-                        }    
-                    </Fragment>
-                )}
+                <img src=
+                { selectedPlace.photos ? (
+                    selectedPlace.photos[0].getUrl()
+                ) : (  selectedPlace.icon )}
+                alt={selectedPlace.name +" image"} className="placeInfoImage" />
+                <p className="placeInfoTitle">
+                    {selectedPlace.name}
+                    <span className={selectedPlace.opening_hours && selectedPlace.opening_hours.open_now ? "statusCircle" : "statusCircle closed"}></span>
+                </p>
+                <p className="placeInfoTitle">
+                    {selectedPlace.rating ? "rating: " + selectedPlace.rating : "No one rated this place"}
+                </p>
+                <p className="placeInfoTitle">
+                    {selectedPlace.vicinity ? selectedPlace.vicinity : "Unknown address"}
+                </p>
+                <button type="button" onClick={ showList }>Go to the list</button>
             </Fragment>
-        );
-    };
-};
+        ) : (
+            <Fragment>
+                { data.places ? (
+                    <List data={data.places} showInfo={showInfo} />
+                    ):(
+                        ""
+                    )
+                }    
+            </Fragment>
+        )}
+    </Fragment>
+);
 
 export default PlaceInfo;
